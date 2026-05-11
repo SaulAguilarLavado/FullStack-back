35f5f9bb3f41605cb6eaf10410a8e64e98002088
SaulAguilarLavado <u22304350@utp.edu.pe>
2026-05-11 15:48:02 -0500

    app

---
+++ src/main/java/com/ticketflow/FullStack_back/FullStackBackApplication.java
@@ -0,0 +1,13 @@
+package com.ticketflow.FullStack_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketflow/FullStack_back/FullStackBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketflow.FullStack_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
